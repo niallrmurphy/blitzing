1d149236ad053763b6d205418ff1c1b1af4675dd
Niall Richard Murphy <niallm@gmail.com>
2020-11-14 22:41:57 +0000

    Tidy up comments

--- it-scrape.py
+++ it-scrape.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
 
+# Parse the Irish Times 400 list, and create data suitable for using as an ersatz search-engine.
+# Note: there is no possibility of an _actual_ aggregator/search engine unless or until a company
+# or person/people put serious effort into standardizing search results, etc etc.
+
 from lxml import html
 import pprint
 import re
 import requests
 import sqlite3
 
+# We write the results to a local DB. Nothing fancy, doesn't have to scale.
 conn = sqlite3.connect('production.db')
 c = conn.cursor()
 
-
-# CREATE TABLE searchDestinations(
-#   destinationID INTEGER PRIMARY KEY,
-#   name TEXT,
-#   description TEXT,
-#   mainURL TEXT,
-#   searchURL TEXT,
-#   tags TEXT
-
+# This vomit is the regex which parses the Irish Times list. I presume a CMS is being used behind the scenes,
+# because the tags surrounding the sites, text, etc, are all confused in a way which strongly suggests
+# a combination of human editing and default decisions by tag-happy CMSes. Neededless to say, there isn't
+# a semantic parsing avaialble.
 link_text_regex="href=\"(.+?)\"><(?:strong|u)>(.+?)<\/(?:strong|\/u)>.?<\/a><br>(.*)<\/p>|href=\"(.+?)\"><u>(.+?)<\/u><\/a><\/strong>(?:.+?)<br>(.*)<\/p>|href=\"(.+?)\" target=(?:.+?)>(.+?)<\/a>(?:.+?)<br>(.+?)<\/p>|href=\"(.+?)\">(.+?)<\/a><\/strong><br>(.+?)<\/p>"
 pattern=re.compile(link_text_regex)
 
+# Go to the IT. Currently this article is not paywalled, as best I can tell.
 page = requests.get('https://www.irishtimes.com/news/consumer/400-yes-400-irish-retailers-for-all-your-online-christmas-shopping-1.4386351')
 tree = html.fromstring(page.content)
 
-#for x in tree.xpath('//a'):
-#    print (html.tostring(x))
-
+# Parse the DOM. XPath is moderately convenient for this, but can't get us all the way,
+# so we gotta use regexes.
 for x in tree.xpath('//*[@id="content_left"]/article/div/section/div/p'):
     para = (html.tostring(x))
     site_details = re.findall(pattern, str(para))
@@ -35,31 +35,23 @@ for x in tree.xpath('//*[@id="content_left"]/article/div/section/div/p'):
         continue
         
     line = list(filter(None, site_details[0]))
+    sitename = line[0].replace('%20', '')
+    description = line[1]
+    url = line[2].replace('%20', '')
+
 
     c.execute("INSERT INTO searchDestinations(name, description, mainURL) VALUES (?, ?, ?)",
-        (line[0].replace('%20', ''),
-        line[1],
-        line[2].replace('%20', '')))
-    #print("INSERT INTO searchDestinations(mainURL, name, description) VALUES (%s, %s, %s)", line[0], line[1], line[2])
-    #print (line)
+        (sitename, description, url))
 
-# Insert a row of data
-
-# Save (commit) the changes
+# Save (commit) the changes, and close.
 conn.commit()
-
-# We can also close the connection if we are done with it.
-# Just be sure any changes have been committed or they will be lost.
 conn.close()
 
-    #link_plus_text = para
-    #link = x.xpath('.//a')
-    # if len(link) != 0:
-    #     for y in link:
-    #         strong = y.xpath('.//strong')
-    #         print (html.tostring(y))
-    #         print (html.tostring(strong))
-
-#for x in tree.xpath('//p[@class="no_name"]'):
-#    for y in tree.
-#    print (html.tostring(x))
+# Format of the to-be-searched sites
+# CREATE TABLE searchDestinations(
+#   destinationID INTEGER PRIMARY KEY,
+#   name TEXT,
+#   description TEXT,
+#   mainURL TEXT,
+#   searchURL TEXT,
+#   tags TEXT
